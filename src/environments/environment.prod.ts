82a979852db6330ba0d68b84aadbba90f6c70c09
Amandine <amandine.watrelos@gmail.com>
2020-05-10 21:02:37 +0200

    add prod firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAZJ5QdFRFjYS7tzdeela9m4tMgt284y6U",
+    authDomain: "timebomb-dfc7e.firebaseapp.com",
+    databaseURL: "https://timebomb-dfc7e.firebaseio.com",
+    projectId: "timebomb-dfc7e",
+    storageBucket: "timebomb-dfc7e.appspot.com",
+    messagingSenderId: "638145898266",
+    appId: "1:638145898266:web:51af6a1d4aa2ce9365eea3",
+    measurementId: "G-CJZBZMQQZN"
+  }
 };
